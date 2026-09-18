b056a6641c676eb91502542528dc4e9bff70740b
threadactor777 <colby.olinarez@gmail.com>
2026-09-17 20:04:01 -0700

    Added REPL to main.go

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Create scanner
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// Infinite for loop
+	for {
+		// Print cursor thingy
+		fmt.Print("Pokedex > ")
+
+		// Scan tokens
+		if scanner.Scan() {
+			input := cleanInput(scanner.Text())
+			// Print first word
+			fmt.Println("Your command was:", input[0])
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Errorf("Error:", err)
+		}
+
+	}
 }
